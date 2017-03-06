5f77fe5410d7d5b7cea7160adbc10d8d4aca14df
Rohan Orton <rohan.orton@gmail.com>
2017-03-06 19:23:51 +0000

    Restart

--- src/Main.elm
+++ src/Main.elm
@@ -1,193 +1,44 @@
 module Main exposing (..)
 
-import Html exposing (div, ul, li, text, Html)
+import Html exposing (div, h1, text, Html)
 import Html.Attributes exposing (style)
-import List exposing (..)
 
 
 main : Html msg
 main =
     div []
-        [ board
-        , piecesHtml startingPieces
+        [ chessboard
         ]
 
 
-piecesHtml : Pieces -> Html a
-piecesHtml rows =
-    rows
-        |> List.map rowToHtml
-        |> div [ style [ ( "font-size", "250%" ), ( "text-align", "center" ), ( "line-height", "1.2" ) ] ]
-
-
-rowToHtml : List (Maybe Piece) -> Html a
-rowToHtml row =
-    let
-        colStyle =
-            ( "display", "inline-block" )
-    in
-        row
-            |> List.map pieceContainer
-            |> div [ style [ colStyle ] ]
-
-
-pieceToText : Maybe Piece -> String
-pieceToText piece =
-    case piece of
-        Just p ->
-            "♙"
-
-        Nothing ->
-            ""
-
-
-pieceContainer maybePiece =
-    let
-        pieceString =
-            pieceToText maybePiece
-
-        sizePx =
-            50
-    in
-        div
-            [ style
-                [ ( "width", (toString sizePx) ++ "px" )
-                , ( "height", (toString sizePx) ++ "px" )
-                ]
+chessboard : Html msg
+chessboard =
+    div
+        [ style
+            [ ( "background-image", "url(/assets/board.svg)" )
+            , ( "height", "360px" )
+            , ( "width", "360px" )
+            , ( "margin", "auto" )
             ]
-            [ text pieceString ]
+        ]
+        [ blackPawn
+        , blackPawn
+        , blackPawn
+        , blackPawn
+        , blackPawn
+        , blackPawn
+        , blackPawn
+        , blackPawn
+        ]
 
 
-board =
-    let
-        colStyle =
-            ( "display", "inline-block" )
-    in
-        div
-            [ style
-                [ ( "font-size", "250%" )
-                , ( "text-align", "center" )
-                , ( "line-height", "1.2" )
-                ]
+blackPawn : Html msg
+blackPawn =
+    div
+        [ style
+            [ ( "background-image", "url(/assets/black-pawn.svg)" )
+            , ( "height", "45px" )
+            , ( "width", "45px" )
             ]
-            (concat
-                (repeat 4
-                    [ div [ style [ colStyle ] ] (concat (repeat 4 [ square "white", square "silver" ]))
-                    , div [ style [ colStyle ] ] (concat (repeat 4 [ square "silver", square "white" ]))
-                    ]
-                )
-            )
-
-
-square color =
-    let
-        sizePx =
-            50
-    in
-        div
-            [ style
-                [ ( "width", (toString sizePx) ++ "px" )
-                , ( "height", (toString sizePx) ++ "px" )
-                , ( "background-color", color )
-                ]
-            ]
-            []
-
-
-
---- CHESS MODEL
-
-
-type Figure
-    = Pawn
-    | Rook
-    | Knight
-    | Bishop
-    | Queen
-    | King
-
-
-type Colour
-    = Black
-    | White
-
-
-type alias Piece =
-    ( Colour, Figure )
-
-
-type alias Pieces =
-    List (List (Maybe Piece))
-
-
-toPiece : Char -> Maybe Piece
-toPiece char =
-    case char of
-        'R' ->
-            Just ( Black, Rook )
-
-        'H' ->
-            Just ( Black, Knight )
-
-        'B' ->
-            Just ( Black, Bishop )
-
-        'Q' ->
-            Just ( Black, Queen )
-
-        'K' ->
-            Just ( Black, King )
-
-        'P' ->
-            Just ( Black, Pawn )
-
-        'r' ->
-            Just ( White, Rook )
-
-        'h' ->
-            Just ( White, Knight )
-
-        'b' ->
-            Just ( White, Bishop )
-
-        'q' ->
-            Just ( White, Queen )
-
-        'k' ->
-            Just ( White, King )
-
-        'p' ->
-            Just ( White, Pawn )
-
-        _ ->
-            Nothing
-
-
-readLine : String -> List (Maybe Piece)
-readLine str =
-    str
-        |> String.toList
-        |> List.map toPiece
-
-
-readPieces : String -> Pieces
-readPieces str =
-    str
-        |> String.trim
-        |> String.split "\n"
-        |> List.map readLine
-
-
-startingPieces : Pieces
-startingPieces =
-    readPieces
-        """
-RHBQKBHR
-PPPPPPPP
-........
-........
-........
-........
-pppppppp
-rhbqkbhr
-        """
+        ]
+        []
